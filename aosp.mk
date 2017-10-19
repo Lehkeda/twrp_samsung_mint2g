d4ccab6a3f0482a86e345b0cb164cead070f5cfd
lehkeda <computer.boy71@yahoo.com>
2017-10-19 14:45:40 +0200

    Rebrand to aosp

--- aosp.mk
+++ aosp.mk
@@ -1,11 +1,11 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/telephony.mk)
+#$(call inherit-product, vendor/cm/config/telephony.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := mint2g
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+#$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/mint2g/full_mint2g.mk)
@@ -17,5 +17,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="mintxx-user 4.1.2 JZO54K S5282XXANF3 release-keys"
     
 
-PRODUCT_NAME := lineage_mint2g
+PRODUCT_NAME := aosp_mint2g
 PRODUCT_DEVICE := mint2g
